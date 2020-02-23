fefbd2787e268fdcccdd1805da988528881cedea
Carsonjt <carsont6541@gmail.com>
2020-02-22 22:32:07 -0800

    .

---
+++ src/main/battlesnake/java/io/starter/TBA/Snake.java
@@ -0,0 +1,31 @@
+
+public class Snake {
+
+	String id;
+	String name;
+	int health;
+	List<Point> bodyLocs;
+
+	public Snake(String id, String name, int health) {
+		bodyLocs = new ArrayList<Point>();
+		this.id = id;
+		this.name = name;
+		this.health = health;
+	}
+
+	public String getID() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public ArrayList<Point> getBodyLocs() {
+		return bodyLocs;
+	}
+	public void addBodyLoc(Point p) {
+		bodyLocs.append(p);
+	}
+}
