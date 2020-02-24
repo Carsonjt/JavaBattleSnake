1ab1ec76ade8e443f5b4fedb10acdff98b963def
Carsonjt <carsonjt6541@gmail.com>
2020-02-24 12:34:36 -0800

    Update JSONParser.java

--- src/main/java/io/battlesnake/starter/JSONParser.java
+++ src/main/java/io/battlesnake/starter/JSONParser.java
@@ -9,35 +9,17 @@ public class JSONParser {
 	
 	public static Board makeBoard(JsonNode json) {
 		Board board = new Board(json.at("/board/height").asInt(), json.at("/board/width").asInt(), json.at("/turn").asInt());
-	
-	
-		JsonNode snakesNode = json.at("/board/snakes");
-		Iterator<String> snakes = snakesNode.fieldNames();
-		System.out.println("INTERATOR TO STRING" + snakes);
-		while(snakes.hasNext()) {
-			JsonNode snake = json.get(snakes.next());
-			System.out.println("1");
-			board.addSnake(new Snake(snake.at("/id").asText(), snake.at("/name").asText(), snake.at("/health").asInt()));
-			System.out.println("2");
-	
+		System.out.println("BEFORE");
+		json.at("/board/snakes").forEach(snake -> {
+			Snake s = new Snake(snake.at("/id").asText(), snake.at("/name").asText(), snake.at("/health").asInt());
+			System.out.println("ADDED SNAKE");
+			snake.at("/body").forEach(body -> {
+				s.addBodyLoc(new Point(body.at("/x").asInt(), body.at("/y").asInt()));
+			System.out.println("ADDED BODY");
+			}
+			board.addSnake(snake);
+		System.out.println("AFTER");
 		}
-		System.out.println("3");
-		//for(Snake sss: board.snakes) {
-		//	System.out.println(sss.name);
-		//}
-		
-		/*JsonNode selfSnake = json.get("/you");
-		Snake self = new Snake(selfSnake.at("/id").asText(), selfSnake.at("/name").asText(), selfSnake.at("/health").asInt());
-		board.setSelf(self);
-		Iterator<String> bodies = selfSnake.at("/body").fieldNames();
-		while(bodies.hasNext()) {
-			JsonNode body = selfSnake.get(bodies.next());
-			self.addBodyLoc(new Point(body.at("/x").asInt(), body.at("/y").asInt()));
-		}
-		self.setHead(self.bodyLocs[0]);
-		self.setNeck(self.bodyLocs[1]);
-		self.setTail(self.bodyLocs[self.bodyLocs.length - 1]);
-		*/
 		return board;
 		
 	}	
