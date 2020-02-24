63be8d52bd1de0df2eed3c5c118d2ddd7ea09e7b
Carsonjt <carsonjt6541@gmail.com>
2020-02-24 12:37:28 -0800

    Update JSONParser.java

--- src/main/java/io/battlesnake/starter/JSONParser.java
+++ src/main/java/io/battlesnake/starter/JSONParser.java
@@ -10,16 +10,20 @@ public class JSONParser {
 	public static Board makeBoard(JsonNode json) {
 		Board board = new Board(json.at("/board/height").asInt(), json.at("/board/width").asInt(), json.at("/turn").asInt());
 		System.out.println("BEFORE");
+		
 		json.at("/board/snakes").forEach(snake -> {
 			Snake s = new Snake(snake.at("/id").asText(), snake.at("/name").asText(), snake.at("/health").asInt());
 			System.out.println("ADDED SNAKE");
+			
 			snake.at("/body").forEach(body -> {
 				s.addBodyLoc(new Point(body.at("/x").asInt(), body.at("/y").asInt()));
-			System.out.println("ADDED BODY");
-			}
-			board.addSnake(snake);
+				System.out.println("ADDED BODY");
+			});
+	
+		board.addSnake(snake);
 		System.out.println("AFTER");
-		}
+		});
+		System.out.println("END");
 		return board;
 		
 	}	
