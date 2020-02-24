1c2a9e7f5dcf55e75d8c81db6f3f02073409d9fe
Carsonjt <carsonjt6541@gmail.com>
2020-02-24 13:43:07 -0800

    Update MoverHead.java

--- src/main/java/io/battlesnake/starter/MoverHead.java
+++ src/main/java/io/battlesnake/starter/MoverHead.java
@@ -12,18 +12,22 @@ public class MoverHead {
 			if(b.snakes[0].bodyLoc[0].getX() == 10)
 				return "up";
 			return "right";
+		}
 		if(b.snakes[0].bodyLoc[0].getX() == 10) {
 			if(b.snakes[0].bodyLoc[0].getY() == 0)
 				return "left";
 			return "up";
+		}
 		if(b.snakes[0].bodyLoc[0].getY() == 0) {
 			if(b.snakes[0].bodyLoc[0].getX() == 0)
 				return "down";
 			return "left";
+		}
 		if(b.snakes[0].bodyLoc[0].getY() == 10) {
 			if(b.snakes[0].bodyLoc[0].getX() == 0)
 				return "right";
 			return "down";
+		}
 		return "down";
 	}
 	
