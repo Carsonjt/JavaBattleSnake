1653e5674c6183c5d4edeba188472831aa02bd9a
Carsonjt <60421988+Carsonjt@users.noreply.github.com>
2020-02-24 17:31:50 -0800

    Update MoverHead.java

--- src/main/java/io/battlesnake/starter/MoverHead.java
+++ src/main/java/io/battlesnake/starter/MoverHead.java
@@ -8,23 +8,23 @@ public class MoverHead {
 	
 	public static String calcMove(Board b) {
 		
-		if(b.snakes[0].bodyLoc[0].getY() == 10) {
-			if(b.snakes[0].bodyLoc[0].getX() == 10)
+		if(b.self.bodyLoc[0].getY() == 10) {
+			if(b.self.bodyLoc[0].getX() == 10)
 				return "up";
 			return "right";
 		}
-		if(b.snakes[0].bodyLoc[0].getX() == 10) {
-			if(b.snakes[0].bodyLoc[0].getY() == 0)
+		if(b.self.bodyLoc[0].getX() == 10) {
+			if(b.self.bodyLoc[0].getY() == 0)
 				return "left";
 			return "up";
 		}
-		if(b.snakes[0].bodyLoc[0].getY() == 0) {
-			if(b.snakes[0].bodyLoc[0].getX() == 0)
+		if(b.self.bodyLoc[0].getY() == 0) {
+			if(b.self.bodyLoc[0].getX() == 0)
 				return "down";
 			return "left";
 		}
-		if(b.snakes[0].bodyLoc[0].getY() == 10) {
-			if(b.snakes[0].bodyLoc[0].getX() == 0)
+		if(b.self.bodyLoc[0].getY() == 10) {
+			if(b.self.bodyLoc[0].getX() == 0)
 				return "right";
 			return "down";
 		}
