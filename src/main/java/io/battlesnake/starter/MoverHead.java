fd12e2e416aa30063d410cc7cbc034bab55e6c23
Carsonjt <60421988+Carsonjt@users.noreply.github.com>
2020-02-25 17:31:39 -0800

    .

--- src/main/java/io/battlesnake/starter/MoverHead.java
+++ src/main/java/io/battlesnake/starter/MoverHead.java
@@ -11,34 +11,14 @@ public class MoverHead {
 	public static String calcMove(Board b) {
 		ArrayList<String> openMoves = calcPossibleMoves(b);
 		
+		HashMap<String, Integer> moveValues = new HashMap<String, Integer>();
+		for(int i = 0; i < openMoves.size(); i++) {
+			moveValues.put(openMoves.get(i), 0);
+		}
 		
-		/*
-		FOLLOW BORDER (CLOCKWISE)
-		
-		if(b.self.bodyLoc[0].getY() == 10) {
-			if(b.self.bodyLoc[0].getX() == 10)
-				return "up";
-			return "right";
-		}
-		if(b.self.bodyLoc[0].getX() == 10) {
-			if(b.self.bodyLoc[0].getY() == 0)
-				return "left";
-			return "up";
-		}
-		if(b.self.bodyLoc[0].getY() == 0) {
-			if(b.self.bodyLoc[0].getX() == 0)
-				return "down";
-			return "left";
-		}
-		if(b.self.bodyLoc[0].getY() == 10) {
-			if(b.self.bodyLoc[0].getX() == 0)
-				return "right";
-			return "down";
-		}
-		return "down";*/
 		Random random = new Random();
 		int index = random.nextInt(openMoves.size());
-		System.out.println(openMoves);
+		System.out.println(moveValues);
 		return openMoves.get(index);
 	}
 	
--- src/main/java/io/battlesnake/starter/MoverUtil.java
+++ src/main/java/io/battlesnake/starter/MoverUtil.java
@@ -24,6 +24,8 @@ public class MoverUtil {
 		return isValid(b, new Point(x, y));
 	}
 	
+	public 
+	
 	
 	
 }
