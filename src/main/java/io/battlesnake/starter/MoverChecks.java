e201752bd26e217d255757616d6b8389e56b25a6
Carsonjt <60421988+Carsonjt@users.noreply.github.com>
2020-03-07 14:52:01 -0800

    Update MoverChecks.java

--- src/main/java/io/battlesnake/starter/MoverChecks.java
+++ src/main/java/io/battlesnake/starter/MoverChecks.java
@@ -134,11 +134,15 @@ public class MoverChecks {
 				for(Point aroundFood: food.getSurrounding()) {
 					for(Snake snakes: p.b.snakes) {
 						if(snakes.head.equals(aroundFood))
-							if(p.b.self.length >= 8 && p.b.self.health > 30)
-								return 0;
 							return 1;
 					}
 				}
+				
+				if(p.b.self.length >= 8 && p.b.self.health > 30) {
+					if(p.isOnBorder())
+						return 0;
+				}
+
 				return -2;
 			}
 		}
