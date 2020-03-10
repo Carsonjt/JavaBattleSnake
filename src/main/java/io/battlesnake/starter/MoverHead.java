3e72c50cc19ea1482ae34375bbde768efceb4175
Carsonjt <60421988+Carsonjt@users.noreply.github.com>
2020-03-09 20:42:35 -0700

    Update MoverHead.java

--- src/main/java/io/battlesnake/starter/MoverHead.java
+++ src/main/java/io/battlesnake/starter/MoverHead.java
@@ -100,8 +100,8 @@ public class MoverHead {
 			moveValues.replace(direction, moveValues.get(direction) + MoverChecks.avoidHeadOnCollision(dPoint));
 			//FORCED HEAD ON COLLISION
 			moveValues.replace(direction, moveValues.get(direction) + MoverChecks.avoidForcedHeadOnCollisions(dPoint));
-			System.out.println("A: " + MoverChecks.avoidMultiForcedHeadOnCollisions(dPoint));
-			moveValues.replace(direction, moveValues.get(direction) + MoverChecks.avoidMultiForcedHeadOnCollisions(dPoint));
+			//System.out.println("A: " + MoverChecks.avoidMultiForcedHeadOnCollisions(dPoint));
+			//moveValues.replace(direction, moveValues.get(direction) + MoverChecks.avoidMultiForcedHeadOnCollisions(dPoint));
 
 			//NON BORDER SPACE
 			moveValues.replace(direction, moveValues.get(direction) + MoverChecks.nonBorderSpace(b, dPoint));
